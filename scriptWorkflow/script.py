2056273716ada257227850af6d95af6b36747f97
Luiz Henrique Marques Gonçalves <65735970+luizmgon@users.noreply.github.com>
2023-04-25 23:40:30 -0300

    Update script.py

--- scriptWorkflow/script.py
+++ scriptWorkflow/script.py
@@ -4,15 +4,15 @@ import os
 def corrige_csv(nome_do_arquivo_entrada):
     print('O arquivo ' + nome_do_arquivo_entrada+ ' foi upado.')
 
-print(os.listdir())
-csv_files = os.listdir('CSV Files')
+//print(os.listdir())
+csv_files = os.listdir('scriptWorkflow/CSV Files')
 
-with open('CSV Files/added_csvs.txt') as f:
+with open('scriptWorkflow/CSV Files/added_csvs.txt') as f:
     added = f.readlines()
 
 for file in csv_files:
     if((file+'\n') not in added):
-        with open('CSV Files/added_csvs.txt', 'a') as f:
+        with open('scriptWorkflow/CSV Files/added_csvs.txt', 'a') as f:
             f.write(file + '\n')
         corrige_csv(file)
         
