4a9aa22dff455f835a63dd82758dcc4351111336
tomasloesch <t224991@dac.unicamp.br>
2023-05-04 18:14:56 -0300

    adiciona busca de escolas ordenadas por medprof

--- front_end/simple_button.js
+++ front_end/simple_button.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.0/firebase-app.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.19.0/firebase-firestore.js'
+import { collection, getDocs, getFirestore, orderBy, query } from 'https://www.gstatic.com/firebasejs/9.19.0/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCy5Pa_k7SaCooNjkLJWG_c0bg07pHS8FQ",
@@ -13,10 +13,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const schoolsRef = collection(db, "Escolas");
+const schoolsRankingQuery = query(schoolsRef, orderBy('medprof', 'desc'));
+const schoolsRanking = await getDocs(schoolsRankingQuery);
+
 document.getElementById("sobreNos").addEventListener("click", () => {
   console.log("Botão 'Sobre nós' clicado");
 });
 
 document.getElementById("rankings").addEventListener("click", () => {
   console.log("Botão 'Rankings' clicado");
+  schoolsRanking.forEach(school => {
+    console.log(school.data());
+  })
 });
