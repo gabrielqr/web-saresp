00dcedf63f4c90c8272fa7370828764ef19efed8
guilherme <guilhermeg12.goncalves@gmail.com>
2023-05-27 15:00:55 -0300

    Adicionando testes para search bar e botao buscar

--- public/test/index.test.js
+++ public/test/index.test.js
@@ -13,6 +13,10 @@ describe("simple_button.html", () => {
       .then(done, done);
   })
   describe("test buttons", () => {
+      it("button element should display 'Buscar'", () => {
+        let element = document.querySelector("#search")
+        expect(element).to.have.text("Buscar")
+      })
       it("button element should display 'Rankings'", () => {
           let element = document.querySelector("#rankings")
           expect(element).to.have.text("Rankings")
@@ -21,5 +25,20 @@ describe("simple_button.html", () => {
           let element = document.querySelector("#sobreNos")
           expect(element).to.have.text("Sobre nós")
       })
+  })
+
+  describe("test search bar", () => {
+    it('should have an input element with the correct attributes', () => {
+      let input = document.querySelector('input');
+      expect(input).to.have.attr('type', 'text');
+      expect(input).to.have.class('search');
+      expect(input).to.have.attr('placeholder', 'Pesquisar...');
+    });
+  
+    it('should update its value when the user types', () => {
+      let input = document.querySelector('input');
+      input.value = 'test';
+      expect(input).to.have.value('test');
+    });
   })
 })
